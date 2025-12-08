f599c8f6d01542c081f06853b2b44e0064bbcc07
Jeremy Taylor <jdt@juxt.pro>
2025-12-08 20:56:22 +0000

    better open frame hook

--- init.el
+++ init.el
@@ -12,22 +12,30 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("d7bf35cbf07fe90b420ca85625d4e1baff08fd64282562dde9dc788ed89c8242" "bbb13492a15c3258f29c21d251da1e62f1abb8bbd492386a673dcfab474186af" "7fd8b914e340283c189980cd1883dbdef67080ad1a3a9cc3df864ca53bdc89cf" default))
+   '("d7bf35cbf07fe90b420ca85625d4e1baff08fd64282562dde9dc788ed89c8242"
+     "bbb13492a15c3258f29c21d251da1e62f1abb8bbd492386a673dcfab474186af"
+     "7fd8b914e340283c189980cd1883dbdef67080ad1a3a9cc3df864ca53bdc89cf"
+     default))
  '(package-selected-packages
-   '(org-download go-mode writeroom-mode writeroom-room writeroom highlight hl-anything highlight-chars fancy-battery theme-changer rainbow-delimiters rainbox-delimiters highlight-parentheses highlight-parentheses-mode flycheck-clj-kondo org-roam pdf-tools winum company magit projectile perspective spacemacs-theme spaceline avy orderless consult marginalia vertico switch-window counsel exec-path-from-shell))
+   '(avy claude-code-ide company consult counsel eat exec-path-from-shell
+	 fancy-battery flycheck-clj-kondo go-mode highlight
+	 highlight-chars highlight-parentheses
+	 highlight-parentheses-mode hl-anything magit marginalia
+	 orderless org-download org-roam pdf-tools perspective
+	 projectile rainbow-delimiters rainbox-delimiters spaceline
+	 spacemacs-theme switch-window theme-changer vertico winum
+	 writeroom writeroom-mode writeroom-room))
+ '(package-vc-selected-packages
+   '((claude-code-ide :url
+		      "https://github.com/manzaltu/claude-code-ide.el")))
  '(safe-local-variable-values
-   '((eval define-clojure-indent
-	   (match 1)
-	   (for-all 1)
-	   (as-> 2))
+   '((eval define-clojure-indent (match 1) (for-all 1) (as-> 2))
      (cider-clojure-cli-aliases . ":xtdb:dev")
      (cider-gradle-parameters . ":clojureRepl")
      (cider-preferred-build-tool . gradle)
      (cider-repl-display-help-banner)
      (eval add-to-list 'cider-test-defining-forms "def-slt-test")
-     (eval define-clojure-indent
-	   (match 1)
-	   (for-all 1))
+     (eval define-clojure-indent (match 1) (for-all 1))
      (cider-ns-refresh-after-fn . "integrant.repl/resume")
      (cider-ns-refresh-before-fn . "integrant.repl/suspend")))
  '(warning-suppress-log-types '((comp))))
